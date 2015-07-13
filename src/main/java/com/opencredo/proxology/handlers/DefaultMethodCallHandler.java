222a7ee51dd393d7b6fe127857e40489d0e887f3
Dominic Fox <dominic.fox@opencredo.com>
2015-07-13 14:00:14 +0100

    Change caching behaviour for default methods

--- src/main/java/com/opencredo/proxology/handlers/DefaultMethodCallHandler.java
+++ src/main/java/com/opencredo/proxology/handlers/DefaultMethodCallHandler.java
@@ -13,33 +13,25 @@ final class DefaultMethodCallHandler {
     private DefaultMethodCallHandler() {
     }
 
-    public static MethodCallHandler forMethod(Method method) {
-        MethodHandle handle = getMethodHandle(method);
+    private static final ConcurrentMap<Method, MethodCallHandler> cache = new ConcurrentHashMap<>();
 
-        return (proxy, args) -> handle.bindTo(proxy).invokeWithArguments(args);
+    public static MethodCallHandler forMethod(Method method) {
+        return cache.computeIfAbsent(method, m -> {
+            MethodHandle handle = getMethodHandle(m);
+
+            return (proxy, args) -> handle.bindTo(proxy).invokeWithArguments(args);
+        });
     }
 
     private static MethodHandle getMethodHandle(Method method) {
         Class<?> declaringClass = method.getDeclaringClass();
-
         try {
-            return privateLookupFor(declaringClass).unreflectSpecial(method, declaringClass);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static final ConcurrentMap<Class<?>, MethodHandles.Lookup> privateLookups = new ConcurrentHashMap<>();
-
-    private static MethodHandles.Lookup privateLookupFor(Class<?> lookupClass) {
-        return privateLookups.computeIfAbsent(lookupClass, DefaultMethodCallHandler::uncachedPrivateLookupFor);
-    }
-
-    private static MethodHandles.Lookup uncachedPrivateLookupFor(Class<?> lookupClass) {
-        try {
-            Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
+            Constructor<MethodHandles.Lookup> constructor = MethodHandles.Lookup.class
+                    .getDeclaredConstructor(Class.class, int.class);
             constructor.setAccessible(true);
-        return constructor.newInstance(lookupClass, MethodHandles.Lookup.PRIVATE);
+
+            return constructor.newInstance(declaringClass, MethodHandles.Lookup.PRIVATE)
+                    .unreflectSpecial(method, declaringClass);
         } catch (IllegalAccessException | NoSuchMethodException | InstantiationException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
